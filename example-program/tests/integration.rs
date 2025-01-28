be3890ffe92b99462b325564a81cd5b7f4aba57f
Jean Marchand <jeanno11@orange.fr>
2025-01-28 16:21:55 +0100

    Switch to LiteSVM

--- example-program/tests/integration.rs
+++ example-program/tests/integration.rs
@@ -1,19 +1,33 @@
+use std::path::PathBuf;
+
+use litesvm::LiteSVM;
 use solana_instruction::{AccountMeta, Instruction};
-use solana_program_test::ProgramTest;
 use solana_pubkey::Pubkey;
 use solana_sdk::{
-    signature::Keypair, signer::Signer, transaction::Transaction,
+    native_token::LAMPORTS_PER_SOL, signature::Keypair, signer::Signer,
+    transaction::Transaction,
 };
 
-#[tokio::test(flavor = "current_thread")]
-async fn no_std_integration() {
-    let mut program_test = ProgramTest::new(
-        "solana_nostd_example_program",
-        solana_nostd_example_program::ID,
-        None,
-    );
-    program_test.prefer_bpf(true);
-    let (banks, payer, recent_blockhash) = program_test.start().await;
+fn read_program() -> Vec<u8> {
+    let mut so_path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+    so_path.push("../target/deploy/solana_nostd_example_program.so");
+
+    std::fs::read(so_path).unwrap()
+}
+
+#[test]
+fn no_std_integration() {
+    let mut svm = LiteSVM::new();
+
+    let payer = Keypair::new();
+    let payer_pk = payer.pubkey();
+
+    svm.airdrop(&payer_pk, LAMPORTS_PER_SOL)
+        .unwrap();
+
+    let program_id = solana_nostd_example_program::ID;
+    let program_bytes = read_program();
+    svm.add_program(program_id, &program_bytes);
 
     let other_user = Keypair::new();
     let accounts = [
@@ -22,19 +36,17 @@ async fn no_std_integration() {
         AccountMeta::new_readonly(Pubkey::default(), false),
     ];
     let instruction = Instruction {
-        program_id: solana_nostd_example_program::ID,
+        program_id,
         accounts: accounts.to_vec(),
         data: vec![],
     };
     let transaction = Transaction::new_signed_with_payer(
         &[instruction],
-        Some(&payer.pubkey()),
+        Some(&payer_pk),
         &[&payer],
-        recent_blockhash,
+        svm.latest_blockhash(),
     );
 
-    banks
-        .process_transaction(transaction)
-        .await
+    svm.send_transaction(transaction)
         .unwrap();
 }
